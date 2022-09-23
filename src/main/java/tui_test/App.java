ab4ed7b5fc7885f1472c5598534d2e142d4b3f5c
Farbkecks <fabian.alich@gmx.de>
2022-09-23 12:00:36 +0200

    draw graphe

--- src/main/java/tui_test/App.java
+++ src/main/java/tui_test/App.java
@@ -7,16 +7,19 @@ public class App {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         var graph = new ArrayList<Coordinate2D>();
-        graph.add(new Coordinate2D(1, 2));
-        graph.add(new Coordinate2D(2, 4));
-        graph.add(new Coordinate2D(3, 8));
-        graph.add(new Coordinate2D(4, 16));
+        graph.add(new Coordinate2D(5, 1));
+        graph.add(new Coordinate2D(7, 2));
+        graph.add(new Coordinate2D(9, 5));
+        graph.add(new Coordinate2D(17, 13));
+        graph.add(new Coordinate2D(27, 13));
+        graph.add(new Coordinate2D(37, 17));
+        graph.add(new Coordinate2D(44, 7));
         var drawer = new Drawer();
-        Coordinate2D oldPoint = new Coordinate2D(1, 1);
-        for (Coordinate2D point : graph) {
-            drawer.setPoint(point, oldPoint);
-            oldPoint.x = point.x;
-            oldPoint.y = point.y;
+        Coordinate2D pointOld = new Coordinate2D(1, 1);
+        for (Coordinate2D pointNew : graph) {
+            drawer.drawBetweenTwoPoints(pointNew, pointOld);
+            pointOld.x = pointNew.x;
+            pointOld.y = pointNew.y;
 
         }
         drawer.print();
--- src/main/java/tui_test/Drawer.java
+++ src/main/java/tui_test/Drawer.java
@@ -8,31 +8,28 @@ public class Drawer {
     int terminalRows, terminalColumns;
     Terminal terminal;
     char sysmble = 'X';
+    char sysmbleMain = 'O';
 
-    void setPoint(Coordinate2D pointNew, Coordinate2D pointOld) throws IOException, InterruptedException {
-        if (pointNew.x >= 1 && pointNew.x <= terminalColumns && pointNew.y >= 1 && pointNew.y <= terminalRows) {
+    void drawBetweenTwoPoints(Coordinate2D pointNew, Coordinate2D pointOld) throws IOException, InterruptedException {
+        if (pointNew.x >= 1 && pointNew.x <= terminalColumns / 3 - 1 && pointNew.y >= 1 && pointNew.y <= terminalRows) {
             int dx = pointNew.x - pointOld.x;
             int dy = pointNew.y - pointOld.y;
 
-            if (dy == 0 || dx == 0) {
-                return;
-            }
-
             int steps = Math.abs(dx) > Math.abs(dy) ? Math.abs(dx) : Math.abs(dy);
-
-            double xinc = dx / steps;
-            double yinc = dy / steps;
+            double xinc = (double) dx / steps;
+            double yinc = (double) dy / steps;
 
             double x = pointOld.x;
             double y = pointOld.y;
 
-            for (int i = 0; i < steps; i++) {
-                terminal.setCursorPosition((int) Math.round(x) * 2, terminalRows - (int) Math.round(y));
+            for (int i = 0; i <= steps; i++) {
+                terminal.setCursorPosition((int) Math.round(x) * 3 + 1, terminalRows - (int) Math.round(y));
                 terminal.putCharacter(sysmble);
                 x += xinc;
                 y += yinc;
             }
-
+            terminal.setCursorPosition(pointNew.x * 3 + 1, terminalRows - pointNew.y);
+            terminal.putCharacter(sysmbleMain);
             terminal.flush();
         }
     }
@@ -47,19 +44,24 @@ public class Drawer {
         DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
         terminal = defaultTerminalFactory.createTerminal();
         var size = terminal.getTerminalSize();
-        terminalRows = size.getRows() - 4;
         for (int i = 0; i < size.getRows(); i++) {
             System.out.println();
         }
+        terminalRows = size.getRows() - 4;
         terminalColumns = size.getColumns() - 30;
 
         for (int i = 0; i < terminalRows; i++) {
             terminal.setCursorPosition(0, i);
-            terminal.putCharacter('|');
+            if (terminalRows - i >= 1 && terminalRows - i <= 9) {
+                terminal.putCharacter(' ');
+            }
+            terminal.putString(String.valueOf(terminalRows - i) + " |");
         }
-        for (int i = 0; i < terminalColumns; i++) {
+        int count = 1;
+        for (int i = 4; i < terminalColumns; i += 3) {
             terminal.setCursorPosition(i, terminalRows);
-            terminal.putCharacter('_');
+            terminal.putString(String.valueOf(count) + " ");
+            count++;
         }
     }
 }
